1beb3092dace549a6a5c01c746d591a363c3d218
Taras Brytskyy <taras@MacBook-Pro.local>
2026-06-12 16:36:44 -0400

    Initial Commit

---
+++ backend/src/main/java/tarasb/spotifysongfinder/SpotifySongFinderApplication.java
@@ -0,0 +1,13 @@
+package tarasb.spotifysongfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifySongFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifySongFinderApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/tarasb/spotifysongfinder/SpotifySongFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package tarasb.spotifysongfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifySongFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
